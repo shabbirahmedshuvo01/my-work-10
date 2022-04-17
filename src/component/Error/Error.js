7e57b7d86df9959ac7b9e7afe9fea09b4e83db07
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-04-18 00:38:31 +0600

    error get fancy

--- src/component/Error/Error.js
+++ src/component/Error/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import lazyCat from '../../images/error/error.jpg'
 
 const Error = () => {
     return (
         <div>
-            <h2>404 this page not found</h2>
+            <h2 className='text-danger m-5'> 🥺404 this page not found 🥺</h2>
+            <br />
+            <img style={{ height: '600px', width: '800px' }} src={lazyCat} alt="" />
         </div>
     );
 };
